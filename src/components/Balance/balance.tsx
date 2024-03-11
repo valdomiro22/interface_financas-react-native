66cdc88d47cd4ebc2ec722a412ee6d0570ee7baa
Valdomiro Santos <valdomiro.jose@outlook.com>
2024-03-11 12:19:00 -0300

    criacao da pagina balance

---
+++ src/components/Balance/balance.tsx
@@ -0,0 +1,14 @@
+import { 
+  View, 
+  Text,
+ } from "react-native";
+import { styles } from "./stylesBalance";
+
+
+export function Balance() {
+  return (
+    <View style={styles.container}>
+      <Text>Balance funcionando</Text>
+    </View>
+  );
+}
