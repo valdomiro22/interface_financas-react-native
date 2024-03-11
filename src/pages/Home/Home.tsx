047e694a003836c8d921ece67123b614deedf2a5
Valdomiro Santos <valdomiro.jose@outlook.com>
2024-03-11 11:32:48 -0300

    criacao da pagina home

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native';
+import { styles } from './stylesHome';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>App rodando</Text>
+      <Text>App rodando</Text>
+      <Text>App rodando</Text>
+      <Text>App rodando</Text>
+      <Text>App rodando</Text>
+      <Text>App rodando</Text>
+      <Text>App rodando</Text>
+      <Text>App rodando</Text>
+    </View>
+  );
+}
